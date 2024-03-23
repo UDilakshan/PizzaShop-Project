b996ad14ae35e5b6f868fd1965088e887c360cf8
Nilusana07 <151917322+Nilusana07@users.noreply.github.com>
2024-03-23 07:54:59 +0530

    Update Header.jsx

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
-import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
+import { MdShoppingBasket, MdAdd, MdLogout, MdShowChart } from "react-icons/md";
 import { motion } from 'framer-motion';
-
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import {app} from "../firebase.config";
-
-
 import Logo from "./../images/Logo.png";
 import Avatar from "./../images/avatar.png";
 import { Link } from "react-router-dom";
 import { useStateValue } from '../context/StateProvider';
 import { actionType } from '../context/reducer';
+import { useEffect } from 'react';
+import CardContainter from '../Container/CardContainter';
 
 
 
@@ -19,9 +18,9 @@ function Header() {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const [{user}, dispatch] = useStateValue();
+const[{user,cartItems,cartShow}, dispatch] = useStateValue();
 
-    const [isMenu, setIsMenu] = useState(false);
+    const [isMenu, setIsMenu] = useState(false)
 
     const login = async () =>{
             if(!user){
@@ -48,13 +47,23 @@ function Header() {
             user: null,
         });
     }
+    const showCart = () => {
+        dispatch({
+          type: actionType.SET_CART_SHOW,
+          cartShow: !cartShow,
+         });
+        };
+    
 
+        useEffect (()=> {},[cartShow]);
   return (
-    <div className='fixed z-50 w-screen h-24 p-3 px-4 md:p-6 md:px-16 cursor-pointer bg-black'>
+    < div className='fixed z-50 w-screen h-24 p-3 px-4 md:p-6 md:px-16 cursor-pointer bg-black' >
+         
+        
         {/* desktop */}
-        <div className='hidden md:flex w-full h-full items-center justify-between'>
+        <div className='hidden md:flex w-full h-full items-center justify-between' >
 
-        <Link to = {"/"} className='flex items-center gap-2'>
+        <Link to = {"/"} className='flex items-center gap-2' >
             <img src={Logo} className='object-cover w-20 -ml-10' alt="Logo" />
             <p className='text-red-100 font-pacifico text-3xl font-bold'>Pizza Shop</p>
         </Link>
@@ -63,11 +72,9 @@ function Header() {
 
         <div className='flex items-center gap-8'>
         <motion.ul 
-        
-        initial = {{opacity:0, x: 200 }} 
-        animate = {{opacity:1, x: 0 }} 
-        exit = {{opacity:0, x: 200 }} 
-        
+            initial = {{opacity:0, x: 200 }} 
+            animate = {{opacity:1, x: 0 }} 
+            exit = {{opacity:0, x: 200 }} 
         className='flex items-center gap-8'>
             <Link to="/">
             <motion.li whileTap={{ scale: 1.3 }} className='text-base text-red-100 hover:text-orange-500 duration-100 
@@ -88,23 +95,24 @@ function Header() {
             transition-all ease-in-out cursor-pointer'onClick={() =>setIsMenu(false)}>Service</motion.li>
             </Link>
         </motion.ul>
+          
+         
+        <div className='relative flex items-center justify-center'>
+            <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' onClick={showCart}/>
+            {cartItems && cartItems.length>0 && (<div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg 
+            flex items-center justify-center'>
+            <p className='text-xs text-white font-semibold'>{cartItems.length}</p>
+            </div>)}
+             </div>
+         
 
-        <Link to="/Carditem">
-          <motion.div className='relative flex items-center justify-center' whileTap={{ scale: 0.7 }}>
-        <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
-        <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
-            <p className='text-xs text-white font-semibold'>2</p>
-        </div>
-          </motion.div>
-        </Link>
-
-
+         <Link to="/Login">
             <div className='relative'>
 
                 <motion.img whileTap={{ scale : 0.6 }}
                 src={user ? user.photoURL : Avatar}
                  className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
-                  alt="Avatar" onClick = {login}/> 
+                  alt="Avatar" /> 
 
             {
                 isMenu && (
@@ -127,17 +135,12 @@ function Header() {
                       </Link>
                   )}
 
-                     <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover: bg-slate-100
-                      transition-all duration-100 ease-in-out text-textColor text-base' onClick = {logout}>
-                      Logout <MdLogout/> </p>
                       
                 </motion.div>
                 )
             }
-
-
-
-            </div>          
+            </div> 
+            </Link >        
 
         </div>
         </div>
@@ -146,31 +149,29 @@ function Header() {
 
 
         {/* mobile */}
-        <div className='flex items-center justify-between md:hidden w-full h-full'>
+        <div className='flex items-center justify-between md:hidden w-full h-full' >
 
-
-        <Link to="/Carditem">
-          <motion.div className='relative flex items-center justify-center' whileTap={{ scale: 0.7 }}>
-        <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
-        <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
-            <p className='text-xs text-white font-semibold'>2</p>
+        <div className='relative flex items-center justify-center'>
+            <MdShoppingBasket className='text-textColor text-2xl cursor-pointer'onClick={showCart} />
+            {cartItems && cartItems.length>0 && (<div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg 
+            flex items-center justify-center'>
+            <p className='text-xs text-white font-semibold'>{cartItems.length}</p>
+            </div>)}
         </div>
-          </motion.div>
-        </Link>
-
+       
 
         <Link to = {"/"} className='flex items-center gap-2'>
             <img src={Logo} className='object-cover w-12' alt="Logo" />
             <p className=' font-pacifico text-slate-100 text-xl font-bold'>Pizza Shop</p>
         </Link>
 
-
+        <Link to="/Login">
         <div className='relative'>
 
             <motion.img whileTap={{ scale : 0.6 }}
             src={user ? user.photoURL : Avatar}
             className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full'
-            alt="Avatar" onClick = {login}/> 
+            alt="Avatar"/> 
 
             {
             isMenu && (
@@ -217,16 +218,16 @@ function Header() {
             </Link>
             </ul>
 
-                <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer
-                 hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base' onClick = {logout} >
-                Logout <MdLogout/> </p>
+                
                 
             </motion.div>
             )
             }
 
         </div> 
+        </Link>
     </div>
+    {cartShow && (<CardContainter/>)} 
 </div>
   )
 }
